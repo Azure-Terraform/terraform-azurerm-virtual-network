0b14fb758da0aad721479ce1bdb70f9b1a51bee3
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-10-19 13:20:14 -0400

    Revert "add -subnet into name"
    This reverts commit 8627356d590075709c039ff9a03e4a4adccf8298.

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.subnet_type}-subnet"
+  name                 = var.subnet_type
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.cidrs
