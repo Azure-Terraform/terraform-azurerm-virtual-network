518f4244bf0e0409148058c8b7ab934d44ea0d27
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-02-01 13:49:20 -0500

    update example

--- example/bastion/main.tf
+++ example/bastion/main.tf
@@ -38,43 +38,29 @@ data "http" "my_ip" {
 data "azurerm_subscription" "current" {
 }
 
-resource "random_string" "random" {
-  length  = 12
-  upper   = false
-  number  = false
-  special = false
-}
-
-resource "random_password" "admin" {
-  for_each    = local.hosts
-  length      = 14
-  special     = true
-}
-
 module "subscription" {
   source = "github.com/Azure-Terraform/terraform-azurerm-subscription-data.git?ref=v1.0.0"
   subscription_id = data.azurerm_subscription.current.subscription_id
 }
 
-module "rules" {
-  source = "git@github.com:openrba/python-azure-naming.git"
+module "naming" {
+  source = "github.com/Azure-Terraform/example-naming-template.git?ref=v1.0.0"
 }
 
 module "metadata" {
   source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.1.0"
 
-  naming_rules = module.rules.yaml
+  naming_rules = module.naming.yaml
 
   market              = "us"
-  project             = "https://gitlab.ins.risk.regn.net/example/"
+  project             = "https://github.com/Azure-Terraform/terraform-azurerm-virtual-network/tree/master/example/bastion"
   location            = "eastus2"
-  sre_team            = "iog-core-services"
   environment         = "sandbox"
-  product_name        = random_string.random.result
-  business_unit       = "iog"
-  product_group       = "core"
+  product_name        = "contosoweb"
+  business_unit       = "infra"
+  product_group       = "contoso"
   subscription_id     = module.subscription.output.subscription_id
-  subscription_type   = "nonprod"
+  subscription_type   = "dev"
   resource_group_type = "app"
 }
 
@@ -89,7 +75,7 @@ module "resource_group" {
 module "virtual_network" {
   source = "../../"
 
-  naming_rules = module.rules.yaml
+  naming_rules = module.naming.yaml
 
   resource_group_name = module.resource_group.name
   location            = module.resource_group.location
