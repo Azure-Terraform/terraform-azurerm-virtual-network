637e9408c8db876b49dc1663bb2ab95e8bd956f2
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-02-12 17:48:24 -0500

    udpate examples

--- example/bastion/main.tf
+++ example/bastion/main.tf
@@ -99,44 +99,7 @@ module "virtual_network" {
                         allow_vnet_inbound       = true
                         allow_vnet_outbound      = true
                       }
-    "iaas-outbound"   = { cidrs = ["10.1.2.0/24"]
-                        allow_vnet_inbound       = true
-                        allow_vnet_outbound      = true
-                        route_table_association  = "default"
-                      }
   }
-
-  route_tables = {
-    default = {
-      disable_bgp_route_propagation = true
-      routes = {
-        internet = {
-          address_prefix         = "0.0.0.0/0"
-          next_hop_type          = "Internet"
-        }
-        internal-1 = { 
-          address_prefix         = "10.0.0.0/8"
-          next_hop_type          = "VirtualAppliance"
-          next_hop_in_ip_address = "1.1.1.1"
-        }
-        internal-2 = {
-          address_prefix         = "172.16.0.0/12"
-          next_hop_type          = "VirtualAppliance"
-          next_hop_in_ip_address = "1.1.1.1"
-        }
-        internal-3 = {
-          address_prefix         = "192.168.0.0/16"
-          next_hop_type          = "VirtualAppliance"
-          next_hop_in_ip_address = "1.1.1.1"
-        }
-        local-vnet = {
-          address_prefix         = "8.8.8.0/24"
-          next_hop_type          = "vnetlocal"
-        }
-      }
-    }
-  }
-
 }
 
 resource "azurerm_public_ip" "bastion" {
