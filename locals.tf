48f618d97c50d7df3d0e1b3417a7caf7bc8c2573
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-03-12 15:07:45 -0500

    cleanup

--- locals.tf
+++ locals.tf
@@ -8,6 +8,8 @@ locals {
   ]
 
   subnets = zipmap(keys(var.subnets), [ for subnet in values(var.subnets): merge(var.subnet_defaults, subnet) ])
-  route_table_associations = {for i, z in local.subnets: i => z.route_table_association if z.route_table_association != ""}
+
+  route_table_associations = {for i, z in local.subnets: i => z.route_table_association if z.route_table_association != null}
+
   peers = zipmap(keys(var.peers), [ for peer in values(var.peers): merge(var.peer_defaults, peer) ])
 }
