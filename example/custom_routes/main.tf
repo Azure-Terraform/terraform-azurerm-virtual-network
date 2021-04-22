d0e54f241913a1113f772051f20e21ccdd22f420
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-04-22 13:41:53 -0400

    add route_tables outputs

--- example/custom_routes/main.tf
+++ example/custom_routes/main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "=2.44.0"
     }
   }
-  required_version = "=0.14.7"
+  required_version = ">=0.14.8"
 }
 
 provider "azurerm" {
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "naming_rules" {
-  description = "naming conventions yaml file" 
+  description = "naming conventions yaml file"
   type        = string
 }
 
-variable "resource_group_name"{
+variable "resource_group_name" {
   description = "Resource group name"
   type        = string
 }
@@ -37,44 +37,44 @@ variable "subnets" {
 
 variable "subnet_defaults" {
   description = "Maps of CIDRs, policies, endpoints and delegations"
-  type        = object({
-                  cidrs                                          = list(string)
-                  enforce_private_link_endpoint_network_policies = bool
-                  enforce_private_link_service_network_policies  = bool
-                  service_endpoints                              = list(string)
-                  delegations                                    = map(object({
-                                                                          name    = string
-                                                                          actions = list(string)
-                                                                       }))
-                  allow_internet_outbound                        = bool   # allow outbound traffic to internet
-                  allow_lb_inbound                               = bool   # allow inbound traffic from Azure Load Balancer
-                  allow_vnet_inbound                             = bool   # allow all inbound from virtual network
-                  allow_vnet_outbound                            = bool   # allow all outbound from virtual network
-                  route_table_association                        = string
-                })
-  default     = { 
-                  cidrs                                          = []
-                  enforce_private_link_endpoint_network_policies = false
-                  enforce_private_link_service_network_policies  = false
-                  service_endpoints                              = []
-                  delegations                                    = {}
-                  allow_internet_outbound                        = false
-                  allow_lb_inbound                               = false
-                  allow_vnet_inbound                             = false
-                  allow_vnet_outbound                            = false
-                  route_table_association                        = null
-                }
+  type = object({
+    cidrs                                          = list(string)
+    enforce_private_link_endpoint_network_policies = bool
+    enforce_private_link_service_network_policies  = bool
+    service_endpoints                              = list(string)
+    delegations = map(object({
+      name    = string
+      actions = list(string)
+    }))
+    allow_internet_outbound = bool # allow outbound traffic to internet
+    allow_lb_inbound        = bool # allow inbound traffic from Azure Load Balancer
+    allow_vnet_inbound      = bool # allow all inbound from virtual network
+    allow_vnet_outbound     = bool # allow all outbound from virtual network
+    route_table_association = string
+  })
+  default = {
+    cidrs                                          = []
+    enforce_private_link_endpoint_network_policies = false
+    enforce_private_link_service_network_policies  = false
+    service_endpoints                              = []
+    delegations                                    = {}
+    allow_internet_outbound                        = false
+    allow_lb_inbound                               = false
+    allow_vnet_inbound                             = false
+    allow_vnet_outbound                            = false
+    route_table_association                        = null
+  }
 }
 
 variable "route_tables" {
   description = "Maps of route tables"
-  type        = map(object({
-                  disable_bgp_route_propagation = bool
-                  routes                        = map(map(string)) 
-                  # keys are route names, value map is route properties (address_prefix, next_hop_type, next_hop_in_ip_address)
-                  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/route_table#route
-                }))
-  default     = {}
+  type = map(object({
+    disable_bgp_route_propagation = bool
+    routes                        = map(map(string))
+    # keys are route names, value map is route properties (address_prefix, next_hop_type, next_hop_in_ip_address)
+    # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/route_table#route
+  }))
+  default = {}
 }
 
 variable "peers" {
@@ -85,19 +85,19 @@ variable "peers" {
 
 variable "peer_defaults" {
   description = "Maps of peer arguments."
-  type        = object({
-                  id                           = string
-                  allow_virtual_network_access = bool
-                  allow_forwarded_traffic      = bool
-                  allow_gateway_transit        = bool
-                  use_remote_gateways          = bool
-                })
-  default     = {
-                  id                           = null    # remote virtual network id
-                  allow_virtual_network_access = true    # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_virtual_network_access
-                  allow_forwarded_traffic      = false   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_forwarded_traffic
-                  allow_gateway_transit        = false   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_gateway_transit
-                  use_remote_gateways          = false   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#use_remote_gateways
-                }
+  type = object({
+    id                           = string
+    allow_virtual_network_access = bool
+    allow_forwarded_traffic      = bool
+    allow_gateway_transit        = bool
+    use_remote_gateways          = bool
+  })
+  default = {
+    id                           = null  # remote virtual network id
+    allow_virtual_network_access = true  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_virtual_network_access
+    allow_forwarded_traffic      = false # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_forwarded_traffic
+    allow_gateway_transit        = false # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#allow_gateway_transit
+    use_remote_gateways          = false # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_peering#use_remote_gateways
+  }
 }
 
