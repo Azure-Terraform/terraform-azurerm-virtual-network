b30162f8de5794278ba30133572552bcf945bd05
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-06-04 13:23:52 -0400

    update provider dependency syntax to v0.13+

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    azurerm = ">= 2.0.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0.0"
+    }
   }
 }
