26622c78539f4cca9307355edc32c542dfeee817
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-06-15 10:54:21 -0400

    allow full range of options for aks subnets

--- locals.tf
+++ locals.tf
@@ -3,9 +3,12 @@ locals {
 
   subnets = zipmap(keys(var.subnets), [ for subnet in values(var.subnets): merge(var.subnet_defaults, subnet) ])
 
-  route_table_associations = {for i, z in local.subnets: i => z.route_table_association if z.route_table_association != null}
+  aks_subnets = (var.aks_subnets == null ? {} : {
+    private = merge(var.subnet_defaults, var.aks_subnets.private)
+    public  = merge(var.subnet_defaults, var.aks_subnets.public)
+  })
 
-  aks_subnets = var.aks_subnets == null ? {} : { private = var.aks_subnets.private, public = var.aks_subnets.public }
+  route_table_associations = {for i, z in local.subnets: i => z.route_table_association if z.route_table_association != null}
 
   peers = zipmap(keys(var.peers), [ for peer in values(var.peers): merge(var.peer_defaults, peer) ])
 
