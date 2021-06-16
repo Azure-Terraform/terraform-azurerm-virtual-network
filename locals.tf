e4fe8aa73662f4cf18d6ea46704312671dbb23d9
Tim Miller <millti01@risk.regn.net>
2021-06-16 12:00:04 -0400

    Fix merge conflict (#32)
    Merge conflict were left in a couple of files and caused the code to be broken. This fixes that.
    
    Co-authored-by: Tim Miller <tim.miller@risk.lexisnexis.com>

--- locals.tf
+++ locals.tf
@@ -3,16 +3,11 @@ locals {
 
   subnets = zipmap(keys(var.subnets), [for subnet in values(var.subnets) : merge(var.subnet_defaults, subnet)])
 
-<<<<<<< HEAD
   aks_subnets = (var.aks_subnets == null ? {} : {
     private = merge(var.subnet_defaults, var.aks_subnets.private)
     public  = merge(var.subnet_defaults, var.aks_subnets.public)
   })
-=======
   route_table_associations = { for i, z in local.subnets : i => z.route_table_association if z.route_table_association != null }
->>>>>>> master
-
-  route_table_associations = {for i, z in local.subnets: i => z.route_table_association if z.route_table_association != null}
 
   peers = zipmap(keys(var.peers), [for peer in values(var.peers) : merge(var.peer_defaults, peer)])
 
