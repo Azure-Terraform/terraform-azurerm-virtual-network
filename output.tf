9e33a6db9c0141d2589d9c250d65b5fb544da4a8
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-06-21 17:29:43 -0400

    match aks output to that of the opinionated module input

--- output.tf
+++ output.tf
@@ -48,30 +48,32 @@ output "subnets" {
   )
 }
 
-output "aks_subnets" {
-  description = "AKS private/public subnet info."
+output "aks" {
+  description = "Virtual network information matching AKS module input."
   value = (var.aks_subnets == null ? null : {
-    private = {
-      id                          = module.aks_subnet["private"].subnet.id
-      resource_group_name         = module.aks_subnet["private"].subnet.resource_group_name
-      address_prefixes            = module.aks_subnet["private"].subnet.address_prefixes
-      service_endpoints           = module.aks_subnet["private"].subnet.service_endpoints
-      network_security_group_id   = module.aks_subnet["private"].nsg_id
-      network_security_group_name = module.aks_subnet["private"].nsg_name
-      virtual_network_name        = azurerm_virtual_network.vnet.name
-      virtual_network_id          = azurerm_virtual_network.vnet.id
-      route_table_id              = azurerm_route_table.aks_route_table.0.id
-    }
-    public = {
-      id                          = module.aks_subnet["public"].subnet.id
-      resource_group_name         = module.aks_subnet["public"].subnet.resource_group_name
-      address_prefixes            = module.aks_subnet["public"].subnet.address_prefixes
-      service_endpoints           = module.aks_subnet["public"].subnet.service_endpoints
-      network_security_group_id   = module.aks_subnet["public"].nsg_id
-      network_security_group_name = module.aks_subnet["public"].nsg_name
-      virtual_network_name        = azurerm_virtual_network.vnet.name
-      virtual_network_id          = azurerm_virtual_network.vnet.id
-      route_table_id              = azurerm_route_table.aks_route_table.0.id
+    subnets = {
+      private = {
+        id                          = module.aks_subnet["private"].subnet.id
+        resource_group_name         = module.aks_subnet["private"].subnet.resource_group_name
+        address_prefixes            = module.aks_subnet["private"].subnet.address_prefixes
+        service_endpoints           = module.aks_subnet["private"].subnet.service_endpoints
+        network_security_group_id   = module.aks_subnet["private"].nsg_id
+        network_security_group_name = module.aks_subnet["private"].nsg_name
+        virtual_network_name        = azurerm_virtual_network.vnet.name
+        virtual_network_id          = azurerm_virtual_network.vnet.id
+        route_table_id              = azurerm_route_table.aks_route_table.0.id
+      }
+      public = {
+        id                          = module.aks_subnet["public"].subnet.id
+        resource_group_name         = module.aks_subnet["public"].subnet.resource_group_name
+        address_prefixes            = module.aks_subnet["public"].subnet.address_prefixes
+        service_endpoints           = module.aks_subnet["public"].subnet.service_endpoints
+        network_security_group_id   = module.aks_subnet["public"].nsg_id
+        network_security_group_name = module.aks_subnet["public"].nsg_name
+        virtual_network_name        = azurerm_virtual_network.vnet.name
+        virtual_network_id          = azurerm_virtual_network.vnet.id
+        route_table_id              = azurerm_route_table.aks_route_table.0.id
+      }
     }
     route_table_id = azurerm_route_table.aks_route_table.0.id
   })
