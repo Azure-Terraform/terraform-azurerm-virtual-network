affb178fb2383d52fc717d19bb6e13a6f793c83f
Wagner Melo <wagnercmelo@gmail.com>
2021-06-30 12:59:33 -0300

    fix nsg rules logic

--- subnet/main.tf
+++ subnet/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_network_security_rule" "deny_all_outbound" {
 }
 
 resource "azurerm_network_security_rule" "allow_lb_inbound" {
-  count                       = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
+  count                       = ((var.create_network_security_group && var.configure_nsg_rules && var.allow_lb_inbound) ? 1 : 0)
 
   name                        = "AllowAzureLoadBalancerIn"
   priority                    = 4095
@@ -86,7 +86,7 @@ resource "azurerm_network_security_rule" "allow_lb_inbound" {
 }
 
 resource "azurerm_network_security_rule" "allow_internet_outbound" {
-  count                       = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
+  count                       = ((var.create_network_security_group && var.configure_nsg_rules && var.allow_internet_outbound) ? 1 : 0)
 
   name                        = "AllowInternetOut"
   priority                    = 4095
@@ -102,7 +102,7 @@ resource "azurerm_network_security_rule" "allow_internet_outbound" {
 }
 
 resource "azurerm_network_security_rule" "allow_vnet_inbound" {
-  count                       = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
+  count                       = ((var.create_network_security_group && var.configure_nsg_rules && var.allow_vnet_inbound) ? 1 : 0)
 
   name                        = "AllowVnetIn"
   priority                    = 4094
@@ -118,7 +118,7 @@ resource "azurerm_network_security_rule" "allow_vnet_inbound" {
 }
 
 resource "azurerm_network_security_rule" "allow_vnet_outbound" {
-  count                       = ((var.create_network_security_group && var.configure_nsg_rules) ? 1 : 0)
+  count                       = ((var.create_network_security_group && var.configure_nsg_rules && var.allow_vnet_outbound) ? 1 : 0)
 
   name                        = "AllowVnetOut"
   priority                    = 4094
