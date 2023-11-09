3e3408492cdbfa8af8fd7ea1e848a91dd7e79472
Joshua Hiscocks <62932202+jhisc@users.noreply.github.com>
2023-11-09 11:14:32 +0000

    Subnet Private Endpoint/Link Updates
    Argument is deprecated

--- subnet/main.tf
+++ subnet/main.tf
@@ -4,8 +4,8 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.cidrs
 
-  enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint_network_policies
-  enforce_private_link_service_network_policies  = var.enforce_private_link_service_network_policies
+  private_endpoint_network_policies_enabled = var.enforce_private_link_endpoint_network_policies
+  private_link_service_network_policies_enabled  = var.enforce_private_link_service_network_policies
 
   service_endpoints = var.service_endpoints
 
