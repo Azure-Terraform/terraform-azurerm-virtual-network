edba690159726cc19b68d466ec5c143233315999
Wayne Carty <wayne.carty@lexisnexisrisk.com>
2024-10-07 13:11:48 -0400

    Adding support for VNET module to work with azurerm 4.x

--- examples/bastion/main.tf
+++ examples/bastion/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.67"
+      version = "~> 4.0.0"
     }
   }
-  required_version = "~> 1.0"
+  required_version = "~> 1.5"
 }
 
 provider "azurerm" {
