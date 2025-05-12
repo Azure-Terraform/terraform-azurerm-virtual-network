72c349bd47357db74799bbe0fe5312a41ae22db1
Venkata Dorisala <dorisalv@risk.regn.net>
2025-05-12 16:06:52 +0100

    fmt run

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ module "subnet" {
   source   = "./subnet"
   for_each = local.subnets
 
-  names               = var.names
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tags                = var.tags
-  use_product_name = var.use_product_name
+  names                = var.names
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  tags                 = var.tags
+  use_product_name     = var.use_product_name
   naming_rules         = var.naming_rules
   enforce_subnet_names = local.enforce_subnet_names
 
