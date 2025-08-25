e2b41a2d998404ce04fa2d2ec60b143884b8def4
JyoshnaMohan <jyoshna.mohan@lexisnexisrisk.com>
2025-08-26 02:17:02 +0530

    Vnet Changes - AKS-02

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
+  name                = (var.virtual_network_name != null && var.virtual_network_name != "" ) ? var.virtual_network_name : "${var.names.product_group}-${var.names.subscription_type}-${var.names.location}-vnet"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "resource_group_name" {
 variable "virtual_network_name" {
   description = "Optional override for the virtual network name. If not set, name is generated from other variables."
   type        = string
-  default     = "vnet"
+  default     = null
 }
 
 variable "location" {
